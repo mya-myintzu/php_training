a831a3801781b939a73e7345e794f344ea069bb5
mya-myintzu <myamyintzuthin12@gmail.com>
2021-12-15 21:06:54 +0630

    [master] Fix review tutorial_02

--- tutorials/tutorial_02/index.php
+++ tutorials/tutorial_02/index.php
@@ -1,28 +1,28 @@
 <!--Create diamond shape with stars-->
 <?php
- $num=6;
-  for($row=0; $row<$num; $row++) {
-    for($col=$row; $col<$num; $col++) {
-      echo "&nbsp &nbsp";
-    }
-    for($col=0; $col<$row-1; $col++) {
-      echo "*  ";
-    }
-    for($col=0; $col<$row; $col++) {
-      echo "*  ";
-    }
-      echo "<br>";
+$num = 6;
+for ($row = 0; $row < $num; $row++) {
+  for ($col = $row; $col < $num; $col++) {
+    echo "&nbsp &nbsp";
   }
+  for ($col = 0; $col < $row - 1; $col++) {
+    echo "*  ";
+  }
+  for ($col = 0; $col < $row; $col++) {
+    echo "*  ";
+  }
+  echo "<br>";
+}
 
-  for($row=0; $row<$num; $row++) {
-    for($col=0; $col<$row; $col++) {
-        echo "&nbsp &nbsp";
-    }
-    for($col=$row; $col<$num; $col++) {
-        echo "* ";
-    }
-    for($col=$row; $col<$num-1; $col++) {
-        echo "* ";
-    }
-    echo "<br>";
+for ($row = 0; $row < $num; $row++) {
+  for ($col = 0; $col < $row; $col++) {
+    echo "&nbsp &nbsp";
   }
+  for ($col = $row; $col < $num; $col++) {
+    echo "* ";
+  }
+  for ($col = $row; $col < $num - 1; $col++) {
+    echo "* ";
+  }
+  echo "<br>";
+}
