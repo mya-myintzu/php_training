0b35ff498c6cdd8becb634f04829fa0c4c3c540e
mya-myintzu <myamyintzuthin12@gmail.com>
2021-12-15 21:05:16 +0630

    [master]Fix review tutorial _01

--- tutorials/tutorial_01/index.php
+++ tutorials/tutorial_01/index.php
@@ -1,38 +1,42 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-<body>
-  <table>
-  <?php
-  for ($row = 1; $row <= 8; $row++) {
-    echo "<tr>";
-    for ($col = 1; $col <= 8; $col++) {
-      if ($col % 2 == 0) {
-        $color = '#fff';
-      } else {
-        $color = '#000';
-      }
-      if ($row % 2 == 0) {
-        if ($col % 2 == 0) {
-          $color = '#000';
-        } else {
-          $color = '#fff';
-        }
-      }
-      echo "<td style='background-color:" . $color . "' > </td>";
 
-      echo "</td>";
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <!--Create chess
+  import css files from css folder-->
+  <table>
+    <?php
+    for ($row = 1; $row <= 8; $row++) {
+      echo "<tr>";
+      for ($col = 1; $col <= 8; $col++) {
+        if ($col % 2 == 0) {
+          $color = '#fff';
+        } else {
+          $color = '#000';
+        }
+        if ($row % 2 == 0) {
+          if ($col % 2 == 0) {
+            $color = '#000';
+          } else {
+            $color = '#fff';
+          }
+        }
+        echo "<td style='background-color:" . $color . "' > </td>";
+
+        echo "</td>";
+      }
+      echo "</tr>";
     }
-    echo "</tr>";
-  }
-  ?>
-</table>
+    ?>
+  </table>
 </body>
+
 </html>
