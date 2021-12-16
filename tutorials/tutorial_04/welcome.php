04909cf04d839575c7839261a36e0a254389f0a7
mya-myintzu <myamyintzuthin12@gmail.com>
2021-12-16 13:33:43 +0630

    [master]tutorial_04 welcome

---
+++ tutorials/tutorial_04/welcome.php
@@ -0,0 +1,22 @@
+<?php
+$uname="admin";
+$pwd="admin";
+
+session_start();
+if(isset($_SESSION['uname'])){
+  echo "<h1>Welcome " .$_SESSION['uname']."</h1>";
+  echo "<br><a href='logout.php'><input type=button value=logout
+  name=logout></a>";
+}
+else{
+  if($_POST['uname']==$uname && $_POST['pwd']==$pwd){
+    $_SESSION['uname']=$uname;
+
+    echo "<script>location.gref='welcome.php'</script>";
+  }
+  else{
+    echo "<script>alert('username or password incorrect!')</script>";
+    echo "<script>location.href='login.php'</script>";
+  }
+}
+?>
