7b13e9274bd565c06fe5919aeac8328577bd0486
mya-myintzu <myamyintzuthin12@gmail.com>
2021-12-18 04:22:09 +0630

    [master]tutorial_05

---
+++ tutorials/tutorial_05/library/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '05ccf243b0eb2b75fedd90c8e99ec34d735bea27',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '05ccf243b0eb2b75fedd90c8e99ec34d735bea27',
+            'dev_requirement' => false,
+        ),
+        'shuchkin/simplexlsx' => array(
+            'pretty_version' => '0.8.31',
+            'version' => '0.8.31.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../shuchkin/simplexlsx',
+            'aliases' => array(),
+            'reference' => '01368371cb5cadf3b986d4c7080c7fdfd0b6f295',
+            'dev_requirement' => false,
+        ),
+    ),
+);
