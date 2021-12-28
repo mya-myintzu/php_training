e5f9e6b037741c1a7f2c849fe1e818ddbcd42fde
mya-myintzu <myamyintzuthin12@gmail.com>
2021-12-28 08:18:37 +0630

    [master]tutorial_10

--- tutorials/tutorial_08/index.php
+++ tutorials/tutorial_08/index.php
@@ -10,20 +10,15 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!--<style>
-        .wrapper{
-            width: 600px;
-            margin: 0 auto;
-        }
-        table tr td:last-child{
-            width: 120px;
-        }
-    </style>-->
+    <script src="js/library/jquery-3.6.0.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
     <script>
         $(document).ready(function() {
             $('[data-toggle="tooltip"]').tooltip();
         });
     </script>
+    
 </head>
 
 <body>
